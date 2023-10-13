8282be6f145e52631be9b99d4b02d35acc7c70e3
michaelwaves <68243408+michaelwaves@users.noreply.github.com>
2023-10-13 22:47:13 +0800

    chat sidebar styling

--- components/ChatsSidebar.tsx
+++ components/ChatsSidebar.tsx
@@ -18,7 +18,7 @@ export default function ChatsSidebar() {
         let chatsArray: any[] = []
         querySnapshot.forEach((doc: any) => {
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+            //console.log(doc.id, " => ", doc.data());
             chatsArray.push({ id: doc.id, ...doc.data() })
         });
         console.log(chatsArray)
@@ -37,7 +37,7 @@ export default function ChatsSidebar() {
 
     const chatSelectorComponents = chats.map((chat: any) => {
         return (
-            <Link href={`/chat/${chat.id}`} className="flex flex-row items-center justify-start gap-2 h-12 w-full px-2 hover:bg-gray-600 rounded-xl ">
+            <Link href={`/chat/${chat.id}`} className="flex flex-row items-center justify-start gap-2 h-12 w-full px-2 hover:bg-gray-700 rounded-xl ">
                 <BsChatLeft className="text-white w-4 h-4" />
                 <p className="text-white">{chat.name}</p>
             </Link>
@@ -45,12 +45,13 @@ export default function ChatsSidebar() {
     })
 
     return (
-        <div className="w-60 h-screen bg-gray-700 p-2">
-            <Link href={"/chat"} className="text-white flex flex-row gap-2 p-2 border-[1px] rounded-md border-gray-400">
+        <div className="w-60 h-screen bg-gray-800 p-2">
+            <Link href={"/chat"} className="text-white items-center flex flex-row gap-2 p-2 border-[1px] rounded-md border-gray-400">
                 <AiOutlinePlus />
                 <p>New Chat</p>
             </Link>
-            <div className="flex flex-col items-center justify-center px-2 py-2">
+            <div className="relative flex flex-col items-center justify-center px-2 py-2">
+                <div className="w-12 h-full absolute top-0 right-0 bg-gradient-to-l from-gray-800"></div>
                 {chats.length > 0 ? chatSelectorComponents : <MoonLoader />}
             </div>
         </div>
